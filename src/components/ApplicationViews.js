d550ad29077ad00390dd609ceb6dda44b94f1cd7
jksiggy <47400504+jksiggy@users.noreply.github.com>
2019-06-10 17:45:22 -0500

    crud party list

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,8 @@ import { Route, Redirect } from 'react-router-dom'
 import React, { Component } from "react"
 import PartyManager from "../modules/PartyManager";
 import PartyList from './party/PartyList'
+import PartyAddForm from './party/PartyAddForm';
+import PartyEdit from './party/PartyEdit'
 
 
 class ApplicationViews extends Component {
@@ -20,7 +22,26 @@ class ApplicationViews extends Component {
            
             this.setState(newState)
         })
-}
+    }
+
+    addParty = party =>
+    PartyManager.post(party)
+        .then(() => PartyManager.getAll())
+        .then(parties =>
+            this.setState({
+                parties: parties
+            })
+        );
+
+        updateParty = (editedPartyObject) => {
+            return PartyManager.put(editedPartyObject)
+                .then(() => PartyManager.getAll())
+                .then(parties => {
+                    this.setState({
+                        parties: parties
+                    })
+                });
+        };
 
     componentDidMount() {
         const newState = {};
@@ -38,6 +59,14 @@ class ApplicationViews extends Component {
                         deleteParty={this.deleteParty}
                         parties={this.state.parties} />
                 }} />
+                <Route path="/parties/new" render={(props) => {
+                    return <PartyAddForm {...props}
+                        addParty={this.addParty} />
+                }} />
+                <Route path="/parties/:partyId(\d+)/edit" render={props => {
+                    return <PartyEdit {...props}  updateParty={this.updateParty} />
+                }}
+                />
 
               </React.Fragment>
           )
---
+++ src/components/party/PartyAddForm.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+
+
+export default class PartyForm extends Component {
+    // Set initial state
+    state = {
+        partyName: "",
+        location: "",
+        date: "",
+    };
+
+    // Update state whenever an input field is edited
+    handleFieldChange = evt => {
+        const stateToChange = {};
+        stateToChange[evt.target.id] = evt.target.value;
+        this.setState(stateToChange);
+    };
+
+    /*
+          Local method for validation, creating party object, and
+          invoking the function reference passed from parent component
+       */
+    constructNewParty = evt => {
+        evt.preventDefault();
+        {
+            const party = {
+                name: this.state.partyName,
+                location: this.state.location,
+                date: this.state.date
+
+            };
+
+            // Create the party and redirect user to party list
+            this.props
+                .addParty(party)
+                .then(() => this.props.history.push("/parties"));
+        }
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <form className="partyForm">
+                    <div className="form-group">
+                        <label htmlFor="partyName">Party Name</label>
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="partyName"
+                            placeholder="Party Name"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="location">location</label>
+                        <input
+                            type="text"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="location"
+                            placeholder="Location"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="date">date</label>
+                        <input
+                            type="date"
+                            required
+                            className="form-control"
+                            onChange={this.handleFieldChange}
+                            id="date"
+                            placeholder="Date"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        onClick={this.constructNewParty}
+                        className="btn btn-primary"
+                    >
+                        Submit
+          </button>
+                </form>
+            </React.Fragment>
+        );
+    }
+}
